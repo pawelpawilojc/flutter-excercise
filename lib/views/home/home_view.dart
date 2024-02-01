0f39f4365db6af0ce4d9acffdbbcbbe9790483aa
Paweł Pawiłojć <pawilojc@gmail.com>
2024-02-01 21:01:18 +0100

    home view placeholder

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-
 import 'package:listopad3/utils/my_colors.dart';
+import 'package:listopad3/utils/my_images.dart';
 import 'package:listopad3/utils/my_texts.dart';
 
-import 'package:listopad3/utils/my_images.dart';
+import 'package:listopad3/views/widgets/social_logo.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -12,50 +12,72 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            Row(
-              children: [
-                Image.asset(MyImages.homeBalloons),
-                Text(
-                  MyTexts.home,
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w700,
-                    color: MyColors.midnightOrchidColor,
-                  ),
-                ),
-                Spacer(),
-                ElevatedButton.icon(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  icon: Icon(
-                    Icons.logout_outlined,
-                    color: MyColors.whiteColor,
-                  ),
-                  label: Text(
-                    MyTexts.logout,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Image.asset(MyImages.homeBalloons),
+                  Text(
+                    MyTexts.home,
                     style: TextStyle(
-                      fontSize: 20,
+                      fontSize: 30,
                       fontFamily: 'Inter',
                       fontWeight: FontWeight.w700,
-                      color: MyColors.whiteColor,
+                      color: MyColors.midnightOrchidColor,
                     ),
                   ),
-                  style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
-                ),
-                Spacer(),
-              ],
-            ),
-            const SizedBox(height: 20),
-            const SingleChildScrollView(
-              child: Column(
-                children: [],
+                  const Spacer(),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(
+                      Icons.logout_outlined,
+                      color: MyColors.whiteColor,
+                    ),
+                    label: Text(
+                      MyTexts.logout,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w700,
+                        color: MyColors.whiteColor,
+                      ),
+                    ),
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                  ),
+                  const Spacer(),
+                ],
               ),
-            ),
-          ],
+              const SizedBox(height: 20),
+              SocialLogo(socialNetwork: 'google'),
+              SocialLogo(socialNetwork: 'fb'),
+              SocialLogo(socialNetwork: 'x'),
+              SocialLogo(socialNetwork: 'in'),
+              SocialLogo(socialNetwork: 'google'),
+              SocialLogo(socialNetwork: 'fb'),
+              SocialLogo(socialNetwork: 'x'),
+              SocialLogo(socialNetwork: 'in'),
+              SocialLogo(socialNetwork: 'google'),
+              SocialLogo(socialNetwork: 'fb'),
+              SocialLogo(socialNetwork: 'x'),
+              SocialLogo(socialNetwork: 'in'),
+              SocialLogo(socialNetwork: 'google'),
+              SocialLogo(socialNetwork: 'fb'),
+              SocialLogo(socialNetwork: 'x'),
+              SocialLogo(socialNetwork: 'in'),
+              SocialLogo(socialNetwork: 'google'),
+              SocialLogo(socialNetwork: 'fb'),
+              SocialLogo(socialNetwork: 'x'),
+              SocialLogo(socialNetwork: 'in'),
+              SocialLogo(socialNetwork: 'google'),
+              SocialLogo(socialNetwork: 'fb'),
+              SocialLogo(socialNetwork: 'x'),
+              SocialLogo(socialNetwork: 'in'),
+            ],
+          ),
         ),
       ),
     );
